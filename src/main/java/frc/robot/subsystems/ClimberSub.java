141c7b706512bdff7218fcaf7997227b9c0900f4
unknown <cmoliverio.10@gmail.com>
2026-02-11 19:14:46 -0600

    hopper and climber sub with talonfxconfigs

--- src/main/java/frc/robot/subsystems/ClimberSub.java
+++ src/main/java/frc/robot/subsystems/ClimberSub.java
@@ -4,11 +4,50 @@
 
 package frc.robot.subsystems;
 
+import org.team4206.battleaid.common.LoadableConfig;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.common.ConfigTalonFX;
+import frc.robot.common.ConfigTalonFX.Config;
 
 public class ClimberSub extends SubsystemBase {
   /** Creates a new ClimberSub. */
-  public ClimberSub() {}
+  /* Configs */
+  ConfigTalonFX.Config climberMotor1Config = new ConfigTalonFX.Config("climberMotor1.toml");
+  ConfigTalonFX.Config climberMotor2Config = new ConfigTalonFX.Config("climberMotor2.toml"); 
+
+  public Config climberConfig; 
+
+  /* Motors */
+  public TalonFX climberMotor1 = new TalonFX(climberMotor1Config.canID, "Default Name"); 
+  public TalonFX climberMotor2 = new TalonFX(climberMotor2Config.canID, "Default Name"); 
+
+  ConfigTalonFX climberMotor1CFGapply = new ConfigTalonFX(climberMotor1Config, climberMotor1);
+  ConfigTalonFX climberMotor2CFGapply = new ConfigTalonFX(climberMotor2Config, climberMotor1);
+
+  public static class Config extends LoadableConfig {
+    
+    /* Positions */
+    public double stowPosition; 
+    public double extendedPosition; 
+    public double climbPosition; 
+    
+    public Config(String filename) {
+      super.load(this, filename); 
+      //LoadableConfig.print(this); 
+    }
+  }
+
+  public ClimberSub(Config climberConfig) {
+    this.climberConfig = climberConfig; 
+
+    climberMotor1CFGapply.setSlot0(climberMotor1Config.slot0);
+    climberMotor2CFGapply.setSlot0(climberMotor2Config.slot0);
+    climberMotor1CFGapply.applyConfigs();
+    climberMotor2CFGapply.applyConfigs();
+  }
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/HopperSub.java
+++ src/main/java/frc/robot/subsystems/HopperSub.java
@@ -4,11 +4,40 @@
 
 package frc.robot.subsystems;
 
+import org.team4206.battleaid.common.LoadableConfig;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.common.ConfigTalonFX;
+import frc.robot.common.ConfigTalonFX.Config;
 
 public class HopperSub extends SubsystemBase {
   /** Creates a new HopperSub. */
-  public HopperSub() {}
+  /* Configs */
+  ConfigTalonFX.Config hopperMotor1Config = new ConfigTalonFX.Config("hopperMotor1.toml"); 
+  ConfigTalonFX.Config hopperMotor2Config = new ConfigTalonFX.Config("hopperMotor2.toml");
+
+  public Config hopperConfig; 
+
+  /* Motors */
+  public TalonFX hopperMotor1 = new TalonFX(hopperMotor1Config.canID, "Default Name"); 
+  public TalonFX hopperMotor2 = new TalonFX(hopperMotor2Config.canID, "Default Name"); 
+
+  public static class Config extends LoadableConfig {
+    /* Misc. */
+    public double intakePercent; 
+    public double outtakePercent; 
+
+    public Config(String filename) {
+      super.load(this, filename); 
+      // LoadableConfig.print(this); 
+    }
+  }
+
+  public HopperSub(Config hopperConfig) {
+    this.hopperConfig = hopperConfig; 
+  }
 
   @Override
   public void periodic() {
